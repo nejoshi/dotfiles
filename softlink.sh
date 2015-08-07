ffc991e8ee2a3cb2cd87045151f6c3043f6cd857
nejoshi <nejoshi@cisco.com>
2015-08-07 15:07:23 -0700

    more customizations

---
+++ softlink.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/.vimrc
+ln -s ~/dotfiles/.vim
+ln -s ~/dotfiles/.exports
+ln -s ~/dotfiles/.bash_prompt
+ln -s ~/dotfiles/.bashrc
+ln -s ~/dotfiles/.bash_profile
+ln -s ~/dotfiles/.hushlogin
+ln -s ~/dotfiles/.aliases
+ln -s ~/dotfiles/.functions
+ln -s ~/dotfiles/.extra
